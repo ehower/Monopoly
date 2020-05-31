cc4bb9935bd0550ceefdc21494b8be116e628137
tuvn789 <55150555+tuvn789@users.noreply.github.com>
2020-05-31 12:23:33 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,4 +1,26 @@
 public class Player
 {
-  
+    private String name;
+    private int space = 0;
+
+    public Player(String n)
+    {
+        name = n;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getSpace()
+    {
+        return space;
+    }
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public void setSpace(int s)
+    {
+        space = s;
+    }
 }
