3e7a20f22335741df4a6f990c4cd562b7618f79c
Jcdiazpeon <44230501+Jcdiazpeon@users.noreply.github.com>
2020-05-31 16:47:17 -0400

    Update MonopolyDriver.java

--- MonopolyDriver.java
+++ MonopolyDriver.java
@@ -1,14 +1,32 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MonopolyDriver extends JPanel
 {
 	static GraphicsConfiguration g;
-	public static void main (String[] args)
+	
+
+	public static void main (String[] args) throws IOException
 	{
-		JFrame frame = new JFrame("Cedar Crest-Opoly",g);
-		createJFrame(frame);
+		JFrame frame = new JFrame("Cedar Crest-Opoly",g);	
+		
+		BufferedImage image = ImageIO.read(new File("Images\\Board.png"));
+		
+		 JPanel pane = new JPanel() 
+		 {
+			 @Override
+	            protected void paintComponent(Graphics g) {
+	                super.paintComponent(g);
+	                g.drawImage(image, 0, 0, null);
+	            }
+	     };
+	     createJFrame(frame);
+	     frame.add(pane);
 	}
 	
 	public static void createJFrame(JFrame frame)
@@ -17,5 +35,6 @@ public class MonopolyDriver extends JPanel
 		frame.setVisible(true);
 		frame.setSize(screenSize.width, screenSize.height);
 	}
+	
 	
 }
