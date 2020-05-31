36c25b04ec134a3306f5069623ec09e84be6a4cc
Jcdiazpeon <44230501+Jcdiazpeon@users.noreply.github.com>
2020-05-31 16:29:47 -0400

    Update MonopolyDriver
    Added a basic JFrame. Only pushing so Cornchip can see it.

--- MonopolyDriver.java
+++ MonopolyDriver.java
@@ -1,8 +1,21 @@
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class MonopolyDriver 
+public class MonopolyDriver extends JPanel
 {
+	static GraphicsConfiguration g;
 	public static void main (String[] args)
 	{
-		
+		JFrame frame = new JFrame("Cedar Crest-Opoly",g);
+		createJFrame(frame);
 	}
+	
+	public static void createJFrame(JFrame frame)
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setVisible(true);
+		frame.setSize(screenSize.width, screenSize.height);
+	}
+	
 }
