f87cbd53e513b544dbf2d98bfc8272abf32d2802
Jcdiazpeon <44230501+Jcdiazpeon@users.noreply.github.com>
2020-05-31 20:55:58 -0400

    Update ProcessingDriver
    Added the first step towards adding players

--- ProcessingDriver.java
+++ ProcessingDriver.java
@@ -4,6 +4,9 @@ import processing.core.PImage;
 public class ProcessingDriver extends PApplet
 {
 	PImage boardImage;
+	int state = 0;
+	String typing = "";
+	int numPlayers = 0;
 	
 	public static void main (String[] args)
 	{
@@ -25,6 +28,49 @@ public class ProcessingDriver extends PApplet
 	public void draw()
 	{
 		image(boardImage,0,0,width/2, width/2);
+		
+		switch(state)
+		{
+			case 0:
+			{
+				textSize(25);
+				text("How many players are in your game? \nHit enter to save or delete to remove text.", width-width/2 + 50, height/2);
+				text(typing, width-width/2 + 50, height - height/3);
+				
+			}
+			break;
+		}
 	}
 	
+	public void keyPressed()
+	{
+		switch(state)
+		{
+			case 0:
+			{
+				 if (key == '\n' ) 
+				 {
+					numPlayers = Integer.parseInt(typing);
+					typing = ""; 
+				 }
+				 
+				 else if (key == DELETE)
+				 {
+					 typing = "";
+				 }
+					    
+				 else 
+				 {
+					typing = typing + key; 
+				 }
+			}
+			break;
+			
+			default:
+				System.out.println("If you are seeing this, Ethan did something very wrong.");
+		}
+			
+			
+			
+		}
 }
