486cee542920b8fefdb2847480cee37828cadfe2
Cornchip <cornchipgonecodin@gmail.com>
2020-05-31 22:45:08 -0400

    C O L O R

--- Property.java
+++ Property.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Image;
 
 public class Property
@@ -6,21 +7,24 @@ public class Property
 	
 	private int houses = 0;
 	private String name;
-	private String color;
+	private Color color;
 	private int houseCost;
 	private Image image;
+	private int cost;
 	
 	private int[] rents;
 	
 	public static final int HOTEL = 5; // 5 houses = 1 hotel
 	
-	public Property(String name, String color, int houseCost, int[] rents, Image image)
+	public Property(String name, Color color, int cost, int houseCost, int[] rents, Image image)
 	{
 		this.name = name;
 		this.color = color;
 		this.rents = rents;
 		this.houseCost = houseCost;
 		this.image = image;
+		
+		this.cost = cost;
 	}
 
 	public int addHouse()
@@ -41,7 +45,7 @@ public class Property
 	public void setHouses(int houses) { this.houses = houses; }
 	
 	public String getName() { return name; }
-	public String getColor() { return color; }
+	public Color getColor() { return color; }
 
 	public int getHouseCost() { return houseCost; }
 	
@@ -49,4 +53,6 @@ public class Property
 	
 	public void setOwner(Player p) { this.owner = p; }
 	public Player getOwner() { return owner; }
+	
+	public int getCost() { return cost; }
 }
